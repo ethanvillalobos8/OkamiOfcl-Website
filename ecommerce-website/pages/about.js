c03674324970dcdc23df500325c965f8f889cc6b
Ethan <ethan.villalobos8@gmail.com>
2022-06-21 00:55:25 -0500

    fixed deployment bug

--- ecommerce-website/pages/about.js
+++ ecommerce-website/pages/about.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from &apos;react&apos;
 
 const About = () => {
   return (
@@ -7,16 +7,16 @@ const About = () => {
             <h3>Thank you.</h3>
             <br></br>
             <p>Thank you for supporting me through a purchase, a website visit, or even just a quick scroll-through of our products.
-                I'm a computer science major and this website you're on was developed and deployed by me. It took a lot of time, and
-                still there's so much I have to add, so much more to learn, and so many more hours ahead of me. I'm proud of how it
-                turned out so far and I'm excited for the day it becomes the e-commerce website and business of my dreams. As you can
-                see, I'm starting off small as it's not easy to scale a business but more is sure to come. So for now, I hope you enjoy
+                I&apos;m a computer science major and this website you&apos;re on was developed and deployed by me. It took a lot of time, and
+                still there&apos;s so much I have to add, so much more to learn, and so many more hours ahead of me. I&apos;m proud of how it
+                turned out so far and I&apos;m excited for the day it becomes the e-commerce website and business of my dreams. As you can
+                see, I&apos;m starting off small as it&apos;s not easy to scale a business but more is sure to come. So for now, I hope you enjoy
                 the stickers I hand draw and digitize to life in photoshop just as much as I do making them.
                 <br></br>
                 <br></br>
                 Oh, and one more thing. Please, if you see any bugs or issues with the website DM me through the instagram link on the
-                footer of the home page and while you're there give us a follow if you haven't already! I want this website to run 
-                smoothly for you guys so I'd love any constructive feedback you have; I'll do my best to try and implement it.
+                footer of the home page and while you&apos;re there give us a follow if you haven&apos;t already! I want this website to run 
+                smoothly for you guys so I&apos;d love any constructive feedback you have; I&apos;ll do my best to try and implement it.
                 <br></br>
                 <br></br>
                 - Ethan
