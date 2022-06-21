1a3a47ad85d051f9a216f96ae6f3725ac28ae2af
Ethan <ethan.villalobos8@gmail.com>
2022-06-21 00:52:11 -0500

    added an about me page and fixed a bug where checkout cancel would redirect to the wrong route + some other style changes

--- ecommerce-website/components/Footer.jsx
+++ ecommerce-website/components/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Link from 'next/link';
 import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 
 const Footer = () => {
   return (
     <>
       <div className="footer-container">
+        <div>
+          {/* <Link className="color" href="/about">Read Me</Link> */}
+        </div>
         <div className="footer-container-policy">
           <p>All purchases are final. No refunds or exchanges.</p>
           <p>
@@ -14,9 +18,11 @@ const Footer = () => {
           </p>
         </div>
         <p>Copyright © 2022 OkamiOfcl All rights reserved</p>
-        <p className="icons">
-          <AiFillInstagram />
-          <AiOutlineTwitter />
+        <p>
+          <AiFillInstagram className="icons"/>
+          <Link href="https://www.instagram.com/okamiofcl/">@okamiofcl</Link>
+          <Link className="color" href="/about"> - Read Me</Link>
+          {/* <AiOutlineTwitter /> */}
         </p>
       </div>
     </>
