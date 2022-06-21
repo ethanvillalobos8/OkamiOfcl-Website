889a847169d5cf8560582b7af2c87f7dee8cd4c9
Ethan <ethan.villalobos8@gmail.com>
2022-06-20 21:02:42 -0500

    updated footer styling

--- ecommerce-website/components/Footer.jsx
+++ ecommerce-website/components/Footer.jsx
@@ -4,15 +4,15 @@ import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 const Footer = () => {
   return (
     <>
-      <div className="footer-container-policy">
-        <p>All purchases are final. No refunds or exchanges.</p>
-        <p>
-          For questions or concerns, please email <a className="footer-container-email" href="mailto:order@example.com">
-            support@okamiofcl.com
-          </a>
-        </p>
-      </div>
       <div className="footer-container">
+        <div className="footer-container-policy">
+          <p>All purchases are final. No refunds or exchanges.</p>
+          <p>
+            For questions or concerns, please email <a className="footer-container-email" href="mailto:order@example.com">
+              support@okamiofcl.com
+            </a>
+          </p>
+        </div>
         <p>Copyright © 2022 OkamiOfcl All rights reserved</p>
         <p className="icons">
           <AiFillInstagram />
